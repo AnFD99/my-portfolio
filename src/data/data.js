237aafb6f414b16bf250a7c30a24a0ff781a289d
Anastasia Fadeeva <anfd.design@gmail.com>
2023-04-29 04:13:33 +0200

    added new portfolio project

--- src/data/data.js
+++ src/data/data.js
@@ -4,52 +4,66 @@ import img3 from '../assets/img/3.png'
 import img4 from '../assets/img/4.png'
 import img5 from '../assets/img/5.png'
 import img6 from '../assets/img/6.png'
+import img7 from '../assets/img/7.png'
 
 
 export const data = [
    {
       id: 1,
+      title: 'Haker News App',
+      img: img7,
+      githubLink: 'https://github.com/AnFD99/news-page',
+      demoLink: 'https://hacker-news-6l98.onrender.com/',
+   },
+   {
+      id: 2,
       title: 'Weather App',
       img: img1,
       githubLink: 'https://github.com/AnFD99/weather-app',
       demoLink: 'https://weather-app-8nfe.onrender.com/',
    },
    {
-      id: 2,
+      id: 3,
       title: 'JWT Auth App',
       img: img2,
       githubLink: 'https://github.com/AnFD99/auth-form',
       demoLink: 'https://auth-app-p13j.onrender.com/',
    },
    {
-      id: 3,
+      id: 4,
       title: 'MoGo',
       img: img3,
       githubLink: 'https://github.com/AnFD99/MoGo',
       demoLink: 'https://anfd99.github.io/MoGo/',
    },
    {
-      id: 4,
+      id: 5,
       title: 'Activebox',
       img: img4,
       githubLink: 'https://github.com/AnFD99/activebox',
       demoLink: 'https://anfd99.github.io/activebox/',
    },
    {
-      id: 5,
+      id: 6,
       title: 'Portfolio for designer',
       img: img5,
       githubLink: 'https://github.com/AnFD99/portfolio',
       demoLink: 'https://anfd99.github.io/portfolio/',
    },
+]
+
+
+export const otherProjects = [
    {
-      id: 6,
+      id: 7,
       title: 'Cuda',
       img: img6,
       githubLink: 'https://github.com/AnFD99/cuda',
       demoLink: 'https://anfd99.github.io/cuda/',
    },
 ]
+
+
 
 
 
